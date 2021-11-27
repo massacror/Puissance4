ca87630d5c79bfe354140911d6596153b8fd9c23
Helena BB <82912539+Helenitee@users.noreply.github.com>
2021-11-27 18:13:07 +0000

    Ex 2 jusqu'à Q2 --> qnd on prend une case est ce qu'il y a un allignement de quatre pions ? Il manque faire ça pour toutes les cases (Q3 non faite)

--- main.java
+++ main.java
@@ -14,17 +14,18 @@ public class projet {
     int j = sc.nextInt(); // nb colonne
     // à chaque partie on va réinicialiser a et j. Comme ça on a une seule variable pour les deux joueurs :D jaj 
 
-    int x = 3; //nb lignes
-    int y = 5; //nb colonnes
+    int x = 3; //num. ligne de la case étudiée
+    int y = 5; //num. colonne de la case étudiée
   }
   public static void javajouer (int a, int j){
     boolean estVide = false;
-    for (int i = grille.length; estPleine != true; i--){ //on boucle du bas de la grille joueur vers le haut; donc de imax en java vers imin
+    for (int i = grille.length; estPleine != true; i--){ 
+      //on boucle du bas de la grille joueur vers le haut; donc de imax en java vers imin
       if (grille[i][j] == 0){ 
       grille[i][j] = a;
       estVide = true;
-      
       }
+      
       else if (grille[i][j] != 0){
         estVide = false;
       }
@@ -34,22 +35,87 @@ public class projet {
 
 //EX2 
   public static boolean aGagneHor(int x, int y, int a){
-    boolean res = true;
+    boolean estComplete = true;
 
-    if(grille[y][x] == 0){res = false;} 
+    if(grille[y][x] != a){estComplete = false;} // on a deux options si la case n'a pas de a(EX: 1): soit elle a un 2 soit elle a un 0. Dans les deux cas on ne peut pas avoir un allignement de 1 si dans la case il n'y a pas de 1.
     // si la case est vide als il ne peut pas y avoir d'allignement. :D
-
-    else if (grille[y][x] == 1){
-      for (int i = 0; i < 4; i++){
-        if (grille[y+i][x] != 1){res = false;} 
-        else if(grille[y][x+1] != 1){res = false;} 
+    
+    else if (grille[y][x] == a){ // on verifie que dans la case il y ait un a pour voir s'il y a un allignement horizonal.
+      if (grille[y][x+1] == a){ // on voit si qnd on avance d'une colonne (sur la même ligne) il y a un 'a'. ==> allignement horizontal vers la droite
+        for (i = 0; i < 3; i++){
+          if (grille[y][x+i] != a){
+            res = false;
+          }
         }
-      res = true;
       }
-    else if (grille[y][x] == 2 ){
-      for (int i = 0; i < 4; i++){
-      if (grille[y+i][x] != 1){res = false;}
+
+      else if(grille[y][x-1] == a){   // Même for mais cette fois on recule d'une colonne .==> allignement horizontal vers la gauche
+        for (i = 0; i < 3; i++){
+          if (grille[y][x-i] != a){
+            res = false;
+          }
+        }
       }
-  return res;
-    }  
+    }
+  return estComplete;
   }
+
+  public static boolean aGagneVer(int x, int y, int a){
+    boolean estComplete = true;
+
+    if(grille[y][x] != a){estComplete = false;} // comme dans la case choisie il n'y a pas de a als il ne peut pas y avoir d'allignement à partir de cette case :l
+    
+    else if (grille[y][x] == a){ // on verifie que dans la case choisie il y ait un a. 
+      if (grille[y+1][x] == a){   // on voit s'il y a un allignement vertical vers le haut
+        for (i = 0; i < 3; i++){
+          if (grille[y+i][x] != a){
+            res = false;
+          }
+        }
+      }
+
+      else if(grille[y-1][x] == a){   // on voit s'il y a un allignement vertical vers le bas
+        for (i = 0; i < 3; i++){
+          if (grille[y-i][x] != a){
+            res = false;
+          }
+        }
+      }
+    }
+  return estComplete;
+  }
+
+
+  public static boolean aGagneDiagMont (int x, int y, int a){
+    boolean estComplete = true;
+
+    if(grille[y][x] != a){estComplete = false;}
+    else if(grille[y+1][x+1] == a) { // on va verifier s'il y a un a dans la diagonale qui monte vers la droite. 
+      for (int i = 0; i < 3; i++){
+        if (grille[y+i][x+i] != a){estComplete = false;}
+      }
+    }
+    else if(grille[y+1][x-1] == a){ // on va verifier s'il y a un a dans la diagonale qui monte vers la gauche. 
+      for (int i = 0; i < 3; i++){
+        if (grille[y+i][x-i] != a){estComplete = false;}
+      }
+    }
+  return estComplete;
+  }
+    public static boolean aGagneDiagDesc (int x, int y, int a){
+    boolean estComplete = true;
+
+    if(grille[y][x] != a){estComplete = false;}
+    else if(grille[y-1][x-1] == a) { // on va verifier s'il y a un a dans la diagonale qui déscend vers la gauche. 
+      for (int i = 0; i < 3; i++){
+        if (grille[y-i][x-i] != a){estComplete = false;}
+      }
+    }
+    else if(grille[y-1][x+1] == a){ // on va verifier s'il y a un a dans la diagonale qui déscend vers la droite. 
+      for (int i = 0; i < 3; i++){
+        if (grille[y-i][x+i] != a){estComplete = false;}
+      }
+    }
+  return estComplete;
+  }
+}
