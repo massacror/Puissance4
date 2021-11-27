016f9cdee5aa964ad7199615371a7d1491136576
Helena BB <82912539+Helenitee@users.noreply.github.com>
2021-11-27 14:07:05 +0000

    EX1

--- main.java
+++ main.java
@@ -1,36 +1,27 @@
+// EX1
 package puissance4;
 
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
+javajouer();
 public class projet {
   public static void initialiseGrille(String[] args){
     int grille[][]={{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},};
     // on inicialise la grille: 6 lignes 7 colonnes
 
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt(); 
-    // pour chaque fois qu'on joue on va réinicialiser a. Comme ça on a une seule variable pour les deux joueurs (et en plus un seur code pour toutes les parties) :D jaj 
-
-    int j = sc.nextInt();
-
+    int a = sc.nextInt(); // nb joueur
+    int j = sc.nextInt(); // nb colonne
+    // à chaque partie on va réinicialiser a et j. Comme ça on a une seule variable pour les deux joueurs :D jaj 
 
   }
-  public static void javajouer (int a, int j, int[][] grille){
+  public static void javajouer (int a, int j, int[][] grille){  // je suis en train de prendre en paramètre aussi grille[][].. jsp si c'est très léal.
     for (int i = 0; i < (grille.length+1); i++){
-      if (grille[i][j] == 0){
+      if (grille[i][j] == 0){ 
       grille[i][j] = a;}
       else i++;
+      // si la case est vide, on remplace le 0 par le nb du joueur. Si elle ne l'est pas, on avance la ligle (sur la même colonne)
     }
   }
-
-  public static int entierAleatoire(int a, int b){
-    //Retourne un entier aléatoire entre a (inclus) et b (inclus)
-    return ThreadLocalRandom.current().nextInt(a, b + 1);	
-    }
-
-  public static void main(String[] args) {
-
-    }
 }
->>>>>>> 819b515ceaecea73ef116658b12793476144b888
