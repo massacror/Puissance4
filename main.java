8a9ff637b1b39bc283f5d782ee591601ffc3c281
Helena BB <82912539+Helenitee@users.noreply.github.com>
2021-11-27 12:06:15 +0000

    Jai fait des hoses

---
+++ main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void initialiseGrille(String[] args) {
+    int grille[][]={{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},};
+  }
+
+  public static char[] afficheGrille (String[] args){}
+}
