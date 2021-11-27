e93883e11802edf95051f05c33346ae4b85ae984
massacror <kimarte.ngo@gmail.com>
2021-11-27 17:31:21 +0100

    correction

--- main.java
+++ main.java
@@ -4,7 +4,6 @@ package puissance4;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-javajouer();
 public class projet {
   public static void initialiseGrille(String[] args){
     int grille[][]={{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},};
@@ -30,9 +29,26 @@ public class projet {
     return ThreadLocalRandom.current().nextInt(a, b + 1);	
     }
 
-    public stat
+    public static void afficheGrille()
+    {
+        for(int i=grille.length; i != -1 ; i--){ // On boucle sur les lignes de la dernière vers la première pour bien afficher
+            if(i!=-1)
+                {
+                    for(int j = 0; j < grille[0].length;j++) // On boucle sur le nb de colonne (Une fois qu'on dans une ligne on l'affiche numéro par numéro)
+                {
+                    System.out.println("|"+grille[i][j]+"|"); //On affiche de haut en bas donc faut boucler sur imax
+                }
+                }
+            else
+                {
+                    for(int j = 0; j < grille[0].length;j++) // On boucle sur le nb de colonne (ligne avec les numéros)
+                        {
+                            System.out.println("|" + j + "|");
+                        }
+                }
+        }}
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
   }
 }
